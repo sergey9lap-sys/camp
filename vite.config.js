bcc720a28201369813e04102da70865bebfa237c
Сергей <9_lap_9@mail.ru>
2026-08-11 13:58:15 +0300

    remove thanks page from main deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
-
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(import.meta.dirname, 'index.html'),
-        thanks: resolve(import.meta.dirname, 'thanks.html'),
-      },
-    },
-  },
+  build: {},
 });
